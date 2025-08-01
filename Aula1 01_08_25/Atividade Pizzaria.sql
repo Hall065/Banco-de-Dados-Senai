39862f2e254dbfefe7d529169f284fee74ec92ec
Hall065 <hall065.2022@gmail.com>
2025-08-01 16:42:48 -0300

    Modelagem + SQL Pizzaria

---
+++ Aula1 01_08_25/Atividade Pizzaria.sql
@@ -0,0 +1,17 @@
+-- Tabela Pizzaria --
+
+create database pizzaria;
+
+use pizzaria;
+
+create table funcionarios (
+id_funcionario int primary key
+);
+
+create table pizzas (
+id_pizza int primary key
+);
+
+create table clientes (
+id_cliente int primary key
+);
